d800d8881cb31d7ec5a6cfa7c3b3385b614d9a81
sasha_litvit <sasha_litvit@mail.ru>
2022-12-04 19:07:13 +0300

    feat: add 4

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,35 @@
+const fs = require('fs')
+const path = require('path')
+
+const pathFolder = path.join(__dirname, 'files_copy')
+
 const copy = async () => {
-    // Write your code here 
+    try{
+         fs.stat(pathFolder,(err, stat)=>{
+            if(err === null){
+                throw new Error('FS operation failed')
+            }
+            else if(err.code === 'ENOENT'){
+                fs.mkdir(pathFolder, (err)=>{
+                    if(err) throw err;
+
+                    fs.readdir(path.join(__dirname, 'files'), (err, files)=>{
+                        if(err) throw err;
+                        files.forEach(file =>{
+                            fs.copyFile(path.join(__dirname, 'files',`${file}`), path.join(__dirname, 'files_copy', `${file}`), (err)=>{
+                                if(err) throw err;
+                            })
+                        })
+                    })
+                })
+                console.log('Folder was create')
+            }
+
+        })
+    } catch(err){
+        console.log(err)
+    }
+    // Write your code here
 };
 
 copy();
