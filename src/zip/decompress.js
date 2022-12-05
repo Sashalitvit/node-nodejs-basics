ba9fb537ee4f2dfb667909fc8561be03b2cbe881
sasha_litvit <sasha_litvit@mail.ru>
2022-12-05 22:02:11 +0300

    feat: zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
-const decompress = async () => {
-    // Write your code here 
-};
+const fs = require('fs');
+const zlib = require('zlib');
+const unzip = zlib.createUnzip()
+const path = require('path');
 
-await decompress();
+const filePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+const fileCompressPath = path.join(__dirname, 'files', 'archiv.gz');
+
+const decompress = () => {
+    try{
+        const input = fs.createReadStream(fileCompressPath);
+        const output = fs.createWriteStream(filePath);
+        input.pipe(unzip).pipe(output);
+    }catch(err){
+        console.log(err)
+    }
+    // Write your code here
+};
+decompress();
