c21f46adb1eb4866887000f13aec7cb5e006196b
sasha_litvit <sasha_litvit@mail.ru>
2022-12-05 22:27:46 +0300

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
-const calculateHash = async () => {
-    // Write your code here 
+Object.create = function(prototype, properties) {
+    if (typeof(prototype) !== 'object') {
+        throw new TypeError();
+   }
+    x = {}
+    if (prototype === null) {
+        x.proto = null;
+    } else {
+      x.proto = prototype;
+    }
+    if (typeof(properties) === 'object') {
+        Object.defineProperties(x, properties);
+    }
+    return x;
+  //Your code goes here
+  //And remember: you need not to reinvent Object.defineProperties on your own!
 };
-
-await calculateHash();
